df879f7e29aabd096678fa727ee039e9c30a2324
Brandon <59942879+btoledo26@users.noreply.github.com>
2023-04-09 00:29:52 -0700

    initial commit

---
+++ QR_Gen/Controllers/DatabaseController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QR_Gen.ViewModels;
+
+namespace QR_Gen.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        private QRFormsDBContext db = new QRFormsDBContext();
+
+        // GET: Database
+        public ActionResult Display()
+        {
+            return View(db.Forms.ToList());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TimeStamp,URLData,phoneNumber,SMSMessage," +
+            "emailAddress,emailSubject,emailBody,firstName,lastName,workNumber,organization," +
+            "houseNumber,street,city,state,zipCode,note")] FormViewModel formViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Forms.Add(formViewModel);
+                db.SaveChanges();
+                return RedirectToAction("Display");
+            }
+
+            return View(formViewModel);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ QR_Gen/Models/FormViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace QR_Gen.ViewModels
+{
+    public class FormViewModel
+    {
+        [Key]
+        public int id { get; set; }
+        public string type { get; set; }
+        public string timestamp { get; set; }
+        public string QRCode { get; set; }
+        public int SelectedColor { get; set; }
+        public string URLData { get; set; }
+        public string phoneNumber { get; set; }
+        public string SMSMessage { get; set; }
+        public string emailAddress { get; set; }
+        public string emailSubject { get; set; }
+        public string emailBody { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string workNumber { get; set; }
+        public string organization { get; set; }
+        public string houseNumber { get; set; }
+        public string street { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipCode { get; set; }
+        public string note { get; set; }
+    }
+
+    public class QRFormsDBContext : DbContext
+    {
+        public QRFormsDBContext() { }
+        public DbSet<FormViewModel> Forms { get; set; }
+    }
+}
